ae9325c9086eb03e2e0d52c094ce0bcbd1c70b61
HoYoung <kua@kakao.com>
2022-01-08 02:31:23 +0900

    로그인 만들기(IDE Change [VSC -> WebStorm])

--- setting/ts/pages/SignUp/index.tsx
+++ setting/ts/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import React, { useCallback, useState, } from 'react';
+import { Link } from 'react-router-dom';
 import { Form, Label, Input, LinkContainer, Button, Header, Error, Success } from './styles';
 
 const SingUp = () => {
@@ -85,7 +86,7 @@ const SingUp = () => {
         </Form>
         <LinkContainer>
           이미 회원이신가요?&nbsp;
-          <a href="/login">로그인 하러가기</a>
+          <Link to="/login">로그인 하러가기</Link>
         </LinkContainer>
       </div>
     );
