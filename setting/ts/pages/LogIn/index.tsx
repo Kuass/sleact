fdbb3d4b465a46cccae72204ee854395bd6e7ecf
HoYoung <kua@kakao.com>
2022-01-09 02:46:33 +0900

    swr(fetcher), webstorm editorconfig

--- setting/ts/pages/LogIn/index.tsx
+++ setting/ts/pages/LogIn/index.tsx
@@ -1,10 +1,13 @@
 import React, {useCallback, useState} from 'react';
 import useInput from "@hooks/useInput";
+import fetcher from "@utils/fetcher";
 import axios from "axios";
 import {Button, Error, Form, Header, Input, Label, LinkContainer} from "@pages/SignUp/styles";
 import {Link} from "react-router-dom";
+import useSWR from 'swr';
 
 const LogIn = () => {
+  const {data, error} = useSWR('/api/users', fetcher);
   const [logInError, setLogInError] = useState(false);
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
@@ -14,43 +17,47 @@ const LogIn = () => {
       e.preventDefault();
       setLogInError(false);
       axios
-          .post(
-              '/api/users/login',
-              { email, password },
-          )
-          .then(() => {
+        .post(
+          '/api/users/login',
+          {email, password},
+          {
+            withCredentials: true
+          },
+        )
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error.response)
+          setLogInError(error.response?.data?.statusCode === 401);
+        });
+    }, [email, password]
+  );
 
-          })
-          .catch((error) => {
-            setLogInError(error.response?.data?.statusCode === 401);
-          });
-      }, [email, password]
-    );
-
-    return (
-      <div id="container">
-        <Header>Sleact</Header>
-        <Form onSubmit={onSubmit}>
-          <Label id="email-label">
-            <span>이메일 주소</span>
-            <div>
-              <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
-            </div>
-          </Label>
-          <Label id="password-label">
-            <span>비밀번호</span>
-            <div>
-              <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
-            </div>
-            {logInError && <Error>이메일 또는 비밀번호가 일치하지 않습니다.</Error>}
-          </Label>
-          <Button type="submit">로그인</Button>
-        </Form>
-        <LinkContainer>
-          아직 회원이 아니신가요?&nbsp;
-          <Link to="/signup">회원가입 하러가기</Link>
-        </LinkContainer>
-      </div>
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail}/>
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword}/>
+          </div>
+          {logInError && <Error>이메일 또는 비밀번호가 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
   );
 };
 
