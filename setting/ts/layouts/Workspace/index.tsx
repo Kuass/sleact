718fba0992aaace1656f830aec8c337ba9d9f485
HoYoung <kua@kakao.com>
2022-01-11 21:41:20 +0900

    프로필 메뉴 만듦(Props, defaultProps, stopPropagation)

--- setting/ts/layouts/Workspace/index.tsx
+++ setting/ts/layouts/Workspace/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useCallback} from "react";
+import React, {FC, useCallback, useState} from "react";
 import useSWR, {mutate} from "swr";
 import fetcher from "@utils/fetcher";
 import axios from "axios";
@@ -6,8 +6,8 @@ import {Redirect, Route, Switch} from "react-router-dom";
 import {
   Channels,
   Chats,
-  Header, MenuScroll,
-  ProfileImg,
+  Header, LogOutButton, MenuScroll,
+  ProfileImg, ProfileModal,
   RightMenu,
   WorkspaceName,
   Workspaces,
@@ -15,11 +15,13 @@ import {
 } from "@layouts/Workspace/styles";
 import gravatar from 'gravatar'
 import loadable from "@loadable/component";
+import Menu from "@components/Menu";
 
 const Channel = loadable(() => import('@pages/Channel'))
 const DirectMessage = loadable(() => import('@pages/DirectMessage'))
 
 const Index: FC = ({children}) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const {data, error, revalidate, mutate} = useSWR('/api/users', fetcher, {
     dedupingInterval: 100000,
   });
@@ -38,11 +40,29 @@ const Index: FC = ({children}) => {
     return <Redirect to="/login"/>
   }
 
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, [])
+
   return (
     <div>
       <Header>
         <RightMenu>
-          <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}/>
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}/>
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                <ProfileModal>
+                  <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}/>
+                  <div>
+                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-active">Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+              </Menu>
+            )}
+          </span>
         </RightMenu>
       </Header>
       <button onClick={onLogout}>로그아웃</button>
