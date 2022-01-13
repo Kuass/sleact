955ae3d2717bff187f9330832eea7955953b8da3
HoYoung <kua@kakao.com>
2022-01-13 18:15:04 +0900

    모달만들기(onCreateWorkspace submit 안됨)

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -11,16 +11,10 @@ L
 setting/ts/webpack.config.ts,2\5\25e4afea3bfe64a629e5a40cf13a40141c237395
 =
 .editorconfig,6\2\626a747c8694eaae66f2afb3d1a745b045782c70
-L
-setting/ts/hooks/useInput.ts,e\5\e546c2f9c0efc627335adc8e72c31cb022b49d23
 R
 "setting/ts/pages/SignUp/styles.tsx,8\7\875bd7a77e43f06b1a8a84d86ad4bb63928e2db9
-L
-setting/ts/package-lock.json,7\9\79e0040c681eb5c81f756bd04db5afc0ff26063a
 ]
 -nest-typeorm/src/channels/channels.service.ts,9\5\95d490b049ca977dfbdb3cc46a05ce969ed1a4d8
-G
-setting/ts/package.json,a\8\a85ed02c379504c6ae0dcb6f0ad9d4f6326693e5
 S
 #nest-typeorm/src/dms/dms.service.ts,1\d\1d28a2bb6eb403e42a9391f53030c5bf62391911
 N
@@ -31,5 +25,3 @@ K
 setting/ts/utils/fetcher.ts,d\5\d5be2c065ee4f8a0ff64c88d11373990f498a777
 H
 setting/ts/typings/db.ts,7\7\77f6b7e850c3414ed156a6beac6956a53fe478ab
-Q
-!setting/ts/pages/SignUp/index.tsx,e\f\efa178ed7e94ebfc83b9ab627fd71c4d9b608052
--- setting/ts/layouts/Workspace/index.tsx
+++ setting/ts/layouts/Workspace/index.tsx
@@ -2,13 +2,14 @@ import React, {FC, useCallback, useState} from "react";
 import useSWR, {mutate} from "swr";
 import fetcher from "@utils/fetcher";
 import axios from "axios";
-import {Redirect, Route, Switch} from "react-router-dom";
+import {Link, Redirect, Route, Switch} from "react-router-dom";
 import {
+  AddButton,
   Channels,
   Chats,
   Header, LogOutButton, MenuScroll,
   ProfileImg, ProfileModal,
-  RightMenu,
+  RightMenu, WorkspaceButton,
   WorkspaceName,
   Workspaces,
   WorkspaceWrapper
@@ -16,13 +17,21 @@ import {
 import gravatar from 'gravatar'
 import loadable from "@loadable/component";
 import Menu from "@components/Menu";
+import {IUser} from "@typings/db";
+import {Button, Input, Label} from "@pages/SignUp/styles";
+import useInput from "@hooks/useInput";
+import Modal from "@components/Modal";
+import {toast} from "react-toastify";
 
 const Channel = loadable(() => import('@pages/Channel'))
 const DirectMessage = loadable(() => import('@pages/DirectMessage'))
 
 const Index: FC = ({children}) => {
   const [showUserMenu, setShowUserMenu] = useState(false);
-  const {data, error, revalidate, mutate} = useSWR('/api/users', fetcher, {
+  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
+  const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('');
+  const [newUrl, onChangeNewUrl, setNewUrl] = useInput('');
+  const {data: userData, error, revalidate, mutate} = useSWR<IUser | false>('/api/users', fetcher, {
     dedupingInterval: 100000,
   });
 
@@ -33,10 +42,14 @@ const Index: FC = ({children}) => {
       withCredentials: true,
     }).then((response) => {
       mutate(false, false);
-    })
+    });
   }, []);
 
-  if (!data) {
+  const onClickCreateWorkspace = useCallback(() => {
+    setShowCreateWorkspaceModal(true);
+  }, []);
+
+  if (!userData) {
     return <Redirect to="/login"/>
   }
 
@@ -44,18 +57,53 @@ const Index: FC = ({children}) => {
     setShowUserMenu((prev) => !prev);
   }, [])
 
+  const onCloseUserProfile = useCallback((e) => {
+    e.stopPropagation();
+    setShowUserMenu(false);
+  }, []);
+
+  const onCreateWorkspace = useCallback((e) => {
+    console.log('ok');
+    e.preventDefault();
+    console.log(newWorkspace);
+    if (!newWorkspace || !newWorkspace.trim()) return;
+    console.log('b');
+    if (!newUrl || !newUrl.trim()) return;
+    console.log('c');
+    axios.post('/api/workspaces', {
+        workspace: newWorkspace,
+        url: newUrl,
+      },
+      {
+        withCredentials: true,
+      },
+    ).then((response) => {
+      revalidate();
+      setShowCreateWorkspaceModal(false);
+      setNewWorkspace('');
+      setNewUrl('');
+    }).catch((error) => {
+      console.dir(error);
+      toast.error(error.response?.data, { position: 'bottom-center' });
+    });
+  }, []);
+
+  const onCloseModal = useCallback(( ) => {
+    setShowCreateWorkspaceModal(false);
+  }, []);
+
   return (
     <div>
       <Header>
         <RightMenu>
           <span onClick={onClickUserProfile}>
-            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}/>
+            <ProfileImg src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname}/>
             {showUserMenu && (
-              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onCloseUserProfile}>
                 <ProfileModal>
-                  <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname}/>
+                  <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname}/>
                   <div>
-                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-name">{userData.nickname}</span>
                     <span id="profile-active">Active</span>
                   </div>
                 </ProfileModal>
@@ -65,11 +113,19 @@ const Index: FC = ({children}) => {
           </span>
         </RightMenu>
       </Header>
-      <button onClick={onLogout}>로그아웃</button>
       <WorkspaceWrapper>
-        <Workspaces>test</Workspaces>
+        <Workspaces>
+          {userData?.Workspaces.map((ws) => {
+            return (
+              <Link key={ws.id} to={`/workspaces/${123}/channel/일반`}>
+                <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
+              </Link>
+            );
+          })}
+          <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
+        </Workspaces>
         <Channels>
-          <WorkspaceName>Sleat</WorkspaceName>
+          <WorkspaceName>Sleact</WorkspaceName>
           <MenuScroll>
             MenuScroll
           </MenuScroll>
@@ -81,6 +137,19 @@ const Index: FC = ({children}) => {
           </Switch>
         </Chats>
       </WorkspaceWrapper>
+      <Modal show={showCreateWorkspaceModal} onCloseModal={onCloseModal}>
+        <form onSubmit={onCreateWorkspace}>
+          <Label id="workspace-label">
+            <span>워크스페이스 이름</span>
+            <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace} />
+          </Label>
+          <Label id="workspace-url-label">
+            <span>워크스페이스 url</span>
+            <Input id="workspace" value={newUrl} onChange={onChangeNewUrl} />
+          </Label>
+          <Button type="submit">생성하기</Button>
+        </form>
+      </Modal>
     </div>
   )
 }
