a99c12501cb794f81277957cee5f48a66d6258cf
HoYoung <kua@kakao.com>
2022-01-19 18:10:53 +0900

    챗 박스 만들기 중간

--- setting/ts/pages/Channel/index.tsx
+++ setting/ts/pages/Channel/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {Container, Header} from "@pages/Channel/styles";
+import ChatBox from "@components/ChatBox";
+import ChatList from "@components/ChatList";
+import UseInput from "@hooks/useInput";
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = UseInput('');
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    setChat('');
+  }, []);
   return (
     <Container>
       <Header>채널</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 }
